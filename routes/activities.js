471bb510cafed34fdecf1927982b004ef79d172e
Apo-P <tolara2000@gmail.com>
2022-04-07 11:39:13 +0300

    Started work on header -Added Logo picture - Edited css - Changed Routes and layouts to correctly display the page title

--- routes/activities.js
+++ routes/activities.js
@@ -13,18 +13,18 @@ router.get('/', (req, res) => {
 
     a1 = {
         name: 'activity1',
-        description: 'Description',
+        description: 'Description1',
         members: 12
     };
 
     a2 = {
         name: 'activity2',
-        description: 'Description',
+        description: 'Description2 this is a super long description basically to test how many character we want in a simple description before it gets to much to show. I think these are enough. But in case they are not here are some more epic character to look at',
         members: 66
     };
 
-
-    res.render('activities', {activities: [a1, a2]});
+    
+    res.render('activities',{title:"ActivitiesTitle" ,activities: [a1, a2]});
 });
 
 router.get('/authors', (req, res) => {
