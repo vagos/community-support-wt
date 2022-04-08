8a05d488c1a9d6d14075176a482335479098ea42
Vagos Labrou <e.lamprou@upnet.gr>
2022-04-08 23:58:24 +0300

    removed debug messages/comments from routers

--- routes/activities.js
+++ routes/activities.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 //middleware that is specific to this router
 router.use((req , res, next) => {
-    console.log('greetings from activities')
-    console.log(`Time:${Date.now()} Request:${req.url}`);
     next();
 });
 
@@ -14,14 +12,10 @@ const controller = require('../controllers/activites');
 router.get('/', (req, res) => {
 
     controller.get_all( (result) => {
-        res.render('activities',{title:"ActivitiesTitle" ,activities: result});
+        res.render('activities', { title:"ActivitiesTitle" ,activities: result } );
     });
 
 
-});
-
-router.get('/authors', (req, res) => {
-    res.sendFile('epic.gif', {root: './public'});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,22 @@ const express = require('express');
 const req = require('express/lib/request');
 
 const router = express.Router();
-const activitiesRouter = require('./activities.js');
-const profileRouter = require('./profile.js');
+const activities_router = require('./activities.js');
+const profile_router = require('./profile.js');
 
 
 //middleware that is specific to this router
 router.use((req , res, next) => {
-    console.log('greetings from index')
-    console.log(`Time:${Date.now()} Request:${req.url}`);
-
     next();
-
 });
 
 //Switch router for /activities
-router.use('/activities',activitiesRouter);
-router.use('/profile',profileRouter);
-/* testing */
+router.use('/activities', activities_router);
+router.use('/profile', profile_router);
 
 //define the home page route
 router.get('/', (req, res) => {
     res.render('home');
 });
-
-router.get('/authors', (req, res) => {
-    res.sendFile('epic.gif', {root: './public'});
-});
-
 
 module.exports = router;
