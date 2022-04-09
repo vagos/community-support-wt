703a3f207ec45060d77d50d8c7e54eec5d92f68d
Vagos Labrou <e.lamprou@upnet.gr>
2022-04-09 20:01:18 +0300

    removed activity posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,14 +1,3 @@
 const db = require('./db');
 
-exports.getActivityPosts = (activityName, cb) => {
-
-    db.connection.query(`
-    SELECT id, name, body FROM posts 
-        WHERE post.activity = 
-        (
-            SELECT activity.id FROM activity WHERE 
-            activity.name = ${activityName}
-        )`, 
-    (err, rows) => { cb(rows) });
-};
 
