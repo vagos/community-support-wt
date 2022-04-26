76b18d54b75076483c3ac478020787c914401f3e
Vagos Labrou <e.lamprou@upnet.gr>
2022-04-26 17:25:30 +0300

    added login page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const req = require('express/lib/request');
 
 const router = express.Router();
 const activitiesRouter = require('./activities.js');
-const profileRouter = require('./profile.js');
-const postRouter = require('./post.js');
+const profileRouter    = require('./profile.js');
+const postRouter       = require('./post.js');
+const loginRouter      = require('./login.js');
 
 
 //middleware that is specific to this router
@@ -16,6 +17,7 @@ router.use((req , res, next) => {
 router.use('/activities', activitiesRouter);
 router.use('/profile', profileRouter);
 router.use('/posts', postRouter);
+router.use('/login', loginRouter);
 
 //define the home page route
 router.get('/', (req, res) => {
