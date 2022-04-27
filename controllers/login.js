5d6cd5220dbe8c9a340785d1d14d544a409c0c40
Vagos Labrou <e.lamprou@upnet.gr>
2022-04-27 23:05:47 +0300

    added signup route. need to add sessions next.

--- controllers/login.js
+++ controllers/login.js
@@ -3,21 +3,20 @@ var LocalStrategy = require('passport-local');
 var crypto = require('crypto');
 var db = require('./db');
 
-salt = 'abc';
-
 passport.use(new LocalStrategy(function verify(username, password, cb) {
 
-  db.connection.query('SELECT * FROM user WHERE username = ?', [ username ], function(err, row) {
-    if (err) { throw err; }
-    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    db.connection.query('SELECT * FROM user WHERE name = ?', [username], function(err, row) {
+        if (err) { throw err; }
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-    crypto.pbkdf2(password, row.salt || salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err); }
-      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      return cb(null, row);
+        row = row[0];
+
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+            return cb(null, row);
+        });
     });
-  });
 }));
-
--- routes/login.js
+++ routes/login.js
@@ -11,16 +11,42 @@ router.get('/', (req, res, next) => {
 
 });
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup');
-});
 
-router.post('/password', 
+router.post('/password',
     passport.authenticate('local', {
 
         sucessRedirect: '/',
         failureRedirect: '/login'
 
-}));
+    }));
+
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+// move this to controller
+const crypto = require('crypto');
+const db = require('../controllers/db');
+
+router.post('/signup', function(req, res, next) { 
+    var salt = crypto.randomBytes(16);
+
+    crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+        if (err) { return next(err); }
+        db.connection.query('INSERT INTO user (name, hashed_password, salt) VALUES (?, ?, ?)', [
+            req.body.username,
+            hashedPassword,
+            salt
+        ], function(err) {
+            if (err) { throw err; }
+            var user = {
+                id: this.lastID,
+                username: req.body.username
+            };
+            res.redirect('/');
+        });
+    });
+});
 
 module.exports = router;
