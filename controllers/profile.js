c472c8121ac31999d52a1021ad527f30aefaf92d
Vagos Labrou <e.lamprou@upnet.gr>
2022-05-07 15:20:14 +0300

    updates to user profile
    quick

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,11 +1,31 @@
 const db = require('./db');
 
-exports.getUserObject = async (username, userId) => {
+exports.getUserObject = async (username, id) => {
    
-    const rows = await db.query(`SELECT * FROM participation JOIN activity
-    ON participation.activity = participation.id
-    WHERE participation.user = ?`, userId);
+    const activities = await db.query(`SELECT * FROM participation JOIN activity
+    ON participation.activity = activity.id
+    WHERE participation.user = ?`, id);
 
-    return rows
+    const friends = await db.query(`SELECT user.id, name FROM friendship JOIN
+    user ON user_second = user.id
+    WHERE user_first = ?`, [id, id]);
+
+    const info = await db.queryOne(`SELECT join_date, bio FROM user WHERE id = ?`, id);
+
+    user = {
+        'name': username,
+        'id': id,
+        'bio': info.bio,
+        'activities': activities,
+        'friends' : friends
+    };
+
+    return user;
+};
+
+exports.changeUserBio = async (id, bio) => {
+    
+    db.query(`UPDATE user SET bio = ?
+    WHERE user.id = ?`, [bio, id]);
 };
 
--- routes/profile.js
+++ routes/profile.js
@@ -8,7 +8,19 @@ router.use((req , res, next) => {
     next();
 });
 
+router.patch('/change-bio', (req, res) => {
+   
+    controller.changeUserBio(req.session.passport.user.id, req.body.bio);
+    res.sendStatus(200);
+
+});
+
 router.get('/', async (req, res) => {
+
+    if (!req.session) {
+        res.render('profile');
+        return
+    }
 
     user = {
         name: req.session.passport.user.username
@@ -16,10 +28,9 @@ router.get('/', async (req, res) => {
 
     console.log(req.session.passport)
 
-    other = await controller.getUserObject(req.session.passport.user.username, req.session.passport.user.id);
-    console.log(other)
+    other = await controller.getUserObject('Anna Triangle', 1);
 
-    res.render('profile', { title:"profile", 'user': user});
+    res.render('profile', { title:'profile', 'user': user});
 });
 
 module.exports = router;
