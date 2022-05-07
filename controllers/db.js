c8f5f5b44c68ee94dda8f74ce02f834a9175c7c4
Vagos Labrou <e.lamprou@upnet.gr>
2022-05-07 15:19:23 +0300

    promisified the mysql query function. need to change it in other places too!

--- controllers/db.js
+++ controllers/db.js
@@ -1,4 +1,5 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+const util = require('util');
 
 const options = {
   host     : process.env.DB_HOST || 'localhost',
@@ -12,6 +13,15 @@ console.log(options);
 
 const connection = mysql.createConnection(options)
 
+const query = util.promisify(connection.query).bind(connection);
+
+async function queryOne(sql, args) {
+    rows = await query(sql, args);
+    return rows[0];
+}
+
 module.exports = {
-    connection : connection
+    connection : connection,
+    query: query,
+    queryOne: queryOne
 }
