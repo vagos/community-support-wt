8cf79bb0c49ddc1952c46cd6ae121ffded38766e
Apo-P <tolara2000@gmail.com>
2022-05-10 21:58:07 +0300

    Made Comments work from db - added new function to posts controller - made post router use new function - made changes to post handlebars

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,16 @@
 const db = require('./db');
 
+// returns post for given id
 exports.getPost = (postId, cb) => {
     db.connection.query(`SELECT * FROM post WHERE 
     post.id = ?`, postId,
     (err, rows) => { if (err) throw err; cb(rows[0]); });
 };
+
+// Returns all comments on given post
+exports.getComments = (postId, cb) => {
+    // Why is the "?" there?
+    db.connection.query(`SELECT * FROM comment WHERE 
+    comment.post = ?`, postId,
+    (err, rows) => {if (err) throw err; cb(rows); });
+}
--- routes/post.js
+++ routes/post.js
@@ -11,12 +11,19 @@ const controller = require('../controllers/posts');
 router.get('/:postId', (req, res) => {
 
     controller.getPost(req.params.postId, (post) => {
-        // test Comments
-        test1 = {id:0, content:"asfasfasf"};
-        test2 = {id:2, content:"aasfafsasfasff"};
-        postInfo = {post:post , comments:[test1,test2]} 
-        res.render('post', postInfo);
-        console.log(postInfo);
+        
+        // retrieve comments
+        console.log(`getting comments for post ${post.id}`);
+
+        controller.getComments(post.id,(comments) => {
+            for (comment of comments){
+                console.log(comment);
+            }
+
+            postInfo = {post:post , comments:comments} ;
+            res.render('post', postInfo);
+            console.log(postInfo);
+        });
     });
 
 });
