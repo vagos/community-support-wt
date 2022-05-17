ae78e9c92cef0369c494a471f436c9ee3db48c75
Apo-P <tolara2000@gmail.com>
2022-05-17 19:47:56 +0300

    Broke up posting utils more for future use

--- public/scripts/posting-util.js
+++ public/scripts/posting-util.js
@@ -4,15 +4,33 @@
 // this will try to post formData given to url given, it will return the server response
 export async function postFormDataAsJson({ url, formData }) {
 
+	let formDataJsonString = await convertFormDataToJson(formData);
+
+	let response = await postJson({url:url, jsonData:formDataJsonString})
+
+    return response;
+
+}
+
+// A simple function that converts form data to json
+export async function convertFormDataToJson(formData) {
+
 	const plainFormData = Object.fromEntries(formData.entries());
 	const formDataJsonString = JSON.stringify(plainFormData);
 
     // console.log(plainFormData);
     // console.log(formDataJsonString);
 
+    return formDataJsonString;
+
+}
+
+// A simple function that post a Json to given url
+export async function postJson({ url, jsonData }) {
+
 	let response = await fetch(url,{
         method : 'PUT',
-        body: formDataJsonString,
+        body: jsonData,
 		headers: {
 			"Content-Type": "application/json",
 			"Accept": "application/json"
