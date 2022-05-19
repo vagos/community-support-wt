fe2be6b65c3e2e08ea1627f100e50c93bf670c68
Vagos Labrou <e.lamprou@upnet.gr>
2022-05-19 21:16:18 +0300

    moved profile picture drawing to util.js

--- public/scripts/post-profile-pics.js
+++ public/scripts/post-profile-pics.js
@@ -1,25 +1,6 @@
 // get all user comment image and name
 const userComments = document.querySelectorAll(".imageAndName");
 
-// will draw the user picture for the given comment node
-function drawProfilePicture(node, username) {
-    ctx = node.getContext('2d');
-
-    seed = numberifyString(username);
-
-    N = Math.min( ( rnd(seed) % 10 ) +  (rnd(seed + 1) % 10) + 1, 10) ;
-
-    for (let i = 0; i < N; i++) {
-        ctx.fillStyle = `rgb(${rnd(seed) % 255}, ${rnd(seed + 1) % 255}, ${rnd(seed + 2) % 255})`;
-        ctx.fillRect(rnd(seed + 1) % 100 + 50,
-            rnd(seed + 4) % 100 + 10, 
-            rnd(seed + 2) % 100 + 50, 
-            rnd(seed + 2) % 100 + 50) 
-
-        seed += 1;
-    }
-}
-
 
 for (user of userComments){
 
