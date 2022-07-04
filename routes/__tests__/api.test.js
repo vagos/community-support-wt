1f42b634966575d9b758b0b03098ddb5faed900d
KonstantoJr <konstantokon29@gmail.com>
2022-07-04 10:58:47 +0300

    added tests for all responses from the api

--- routes/__tests__/api.test.js
+++ routes/__tests__/api.test.js
@@ -2,10 +2,51 @@ const request = require('supertest');
 const app = require('../../index');
 
 
-test('return user', async () => {
+test('return users', async () => {
 
     const res = await request(app)
         .get('/api/users');
 
     expect(res.statusCode).toEqual(200);
 });
+
+
+test('return a specific user', async () => {
+
+    const res = await request(app)
+        .get('/api/users/2');
+
+    expect(res.statusCode).toEqual(200);
+});
+
+test('return all the posts', async () => {
+
+    const res = await request(app)
+        .get('/api/posts');
+
+    expect(res.statusCode).toEqual(200);
+})
+
+test('return a specific post', async () => {
+
+    const res = await request(app)
+        .get('/api/posts/1');
+
+    expect(res.statusCode).toEqual(200);
+})
+
+test('return all activities', async () => {
+
+    const res = await request(app)
+        .get('/api/activity');
+
+    expect(res.statusCode).toEqual(200);
+})
+
+test('return a specific activity', async () => {
+
+    const res = await request(app)
+        .get('/api/activity/1');
+
+    expect(res.statusCode).toEqual(200);
+})
